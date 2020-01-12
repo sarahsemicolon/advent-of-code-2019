956a5db88324b621b41acc2738c2d3db31ee4ad0
Sarah Beharry <sarahbeharry@hotmail.com>
2020-01-12 19:02:20 +0000

    Day 5  Part 1 solution, with tests

--- src/main/scala/intcode/Computer.scala
+++ src/main/scala/intcode/Computer.scala
@@ -1,38 +1,61 @@
 package intcode
 
+import scala.collection.mutable.ListBuffer
 import scala.util.control.Breaks.{break, breakable}
 
 object Computer {
-  var opCodeInstructionLength: Map[Int, Int] = Map(
+  val immediateMode = '1'
+
+  val opCodeInstructionLength = Map(
     1 -> 4,
-    2 -> 4)
+    2 -> 4,
+    3 -> 2,
+    4 -> 2)
 
   def runProgram (input : Seq[Int], inputInstruction : Int = 0) : (Seq[Int], Seq[Int]) = {
     var code = input
     var position = 0
-    var output = Seq()
+    var output = new ListBuffer[Int]()
     breakable { while (position < code.length) {
-      if (code(position) == 99) break
+      if (code(position) equals 99) break
+      breakable {
+        var instruction = code(position).toString
+        val opCode = instruction.takeRight(2).toInt
+        instruction = instruction.reverse.padTo(opCodeInstructionLength(opCode) + 1, "0").reverse.mkString
 
-      var instruction = code(position).toString
-      val opCode = instruction.takeRight(2).toInt
-      instruction = instruction.reverse.padTo(opCodeInstructionLength(opCode), "0").reverse.mkString
+        val mode1 = instruction(instruction.length - 3)
+        val param1 = if (mode1 equals immediateMode) code(position + 1) else code(code(position + 1))
+//        val param1 = code(position + 1)
 
-      val mode1 = instruction(instruction.length - 2)
-      val mode2 = instruction(instruction.length - 3)
+        opCode match {
+          case 3 =>
+            code = code.updated(code(position + 1), inputInstruction)
+            position += opCodeInstructionLength(opCode)
+            break
+          case 4 =>
+            output += param1
+            position += opCodeInstructionLength(opCode)
+            break
+          case _ =>
+        }
 
-      val param1 = if (mode1 == 1) code(position + 1) else code(code(position + 1))
-      val param2 = if (mode2 == 1) code(position + 2) else code(code(position + 2))
-      val indexToEdit = code(position + 3)
+        val mode2 = instruction(instruction.length - 4)
+        val param2 = if (mode2 equals immediateMode) code(position + 2) else code(code(position + 2))
 
-      opCode match {
-        case 1 => code = code.updated(indexToEdit, param1 + param2)
-        case 2 => code = code.updated(indexToEdit, param1 * param2)
+        val mode3 = instruction(instruction.length - 5)
+        val param3 = code(position + 3)
+
+        opCode match {
+          case 1 =>
+            code = code.updated(param3, param1 + param2)
+            position += opCodeInstructionLength(opCode)
+          case 2 =>
+            code = code.updated(param3, param1 * param2)
+            position += opCodeInstructionLength(opCode)
+        }
       }
-
-      position += opCodeInstructionLength(opCode)
     }}
-    (code, output)
+    (code, output.toSeq)
   }
 
   def runProgramWithAdjustment(input: Seq[Int], noun: Int, verb: Int) : (Seq[Int], Seq[Int]) =
