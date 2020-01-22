65df52778a0be7653b94a754b658c92926a03e0f
Sarah Beharry <sarahbeharry@hotmail.com>
2020-01-22 09:42:02 +0000

    Rename some fields in Computer

--- src/main/scala/intcode/Computer.scala
+++ src/main/scala/intcode/Computer.scala
@@ -16,36 +16,36 @@ object Computer {
     7 -> 4,
     8 -> 4)
 
-  var code = Seq[Int]()
+  var software = Seq[Int]()
   var instruction = ""
   var position = 0
 
   def getParam(paramPosition: Int, forceImmediateMode: Boolean = false) = {
     val mode = instruction(instruction.length - (2 + paramPosition))
-    if (forceImmediateMode || (mode equals immediateMode)) code(position + paramPosition) else code(code(position + paramPosition))
+    if (forceImmediateMode || (mode equals immediateMode)) software(position + paramPosition) else software(software(position + paramPosition))
   }
 
-  def runProgram (input : Seq[Int], inputInstruction : Int = 0) : (Seq[Int], Seq[Int]) = {
-    code = input
+  def runProgram (softwareParam : Seq[Int], inputInstruction : Int = 0) : (Seq[Int], Seq[Int]) = {
+    software = softwareParam
     position = 0
     var output = new ListBuffer[Int]()
-    breakable { while (position < code.length) {
-      if (code(position) equals 99) break
+    breakable { while (position < software.length) {
+      if (software(position) equals 99) break
       breakable {
-        instruction = code(position).toString
+        instruction = software(position).toString
         val opCode = instruction.takeRight(2).toInt
         instruction = instruction.reverse.padTo(opCodeInstructionLength(opCode) + 1, "0").reverse.mkString
 
         opCode match {
           case 1 =>
-            code = code.updated(getParam(3, true), getParam(1) + getParam(2))
+            software = software.updated(getParam(3, true), getParam(1) + getParam(2))
             position += opCodeInstructionLength(opCode)
           case 2 =>
-            code = code.updated(getParam(3, true), getParam(1) * getParam(2))
+            software = software.updated(getParam(3, true), getParam(1) * getParam(2))
             position += opCodeInstructionLength(opCode)
           case 3 =>
             // always use immediate mode
-            code = code.updated(getParam(1, true), inputInstruction)
+            software = software.updated(getParam(1, true), inputInstruction)
             position += opCodeInstructionLength(opCode)
             break
           case 4 =>
@@ -66,19 +66,19 @@ object Computer {
               position += opCodeInstructionLength(opCode)
             }
           case 7 =>
-            code = code.updated(getParam(3, true),
+            software = software.updated(getParam(3, true),
               if (getParam(1) < getParam(2)) 1 else 0)
             position += opCodeInstructionLength(opCode)
             break
           case 8 =>
-            code = code.updated(getParam(3, true),
+            software = software.updated(getParam(3, true),
               if (getParam(1).equals(getParam(2))) 1 else 0)
             position += opCodeInstructionLength(opCode)
             break
         }
       }
     }}
-    (code, output.toSeq)
+    (software, output.toSeq)
   }
 
   def runProgramWithAdjustment(input: Seq[Int], noun: Int, verb: Int) : (Seq[Int], Seq[Int]) =
