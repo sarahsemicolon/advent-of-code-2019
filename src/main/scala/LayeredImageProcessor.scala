0e30d98e89bc7f10f40a76fd7444e15096e47806
Sarah Beharry <sarahbeharry@hotmail.com>
2020-01-22 16:53:43 +0000

    Day 8 Part 2

--- src/main/scala/LayeredImageProcessor.scala
+++ src/main/scala/LayeredImageProcessor.scala
@@ -3,4 +3,9 @@ class LayeredImageProcessor {
     val layerWithLeastZeros = imageData grouped(size._1 * size._2) minBy (x => x.count(_ == '0'))
     layerWithLeastZeros.count(_ == '1') * layerWithLeastZeros.count(_ == '2')
   }
+
+  def decodeImage(imageData: String, size: (Int, Int)) : String = {
+    val pixelLists = imageData.grouped (size._1 * size._2).map(x => x.toList).toList.transpose
+    pixelLists.map(x => x.find(i => i != '2')).flatten.mkString.grouped(size._1).mkString("\n")
+  }
 }
--- src/test/scala/LayeredImageProcessorTest.scala
+++ src/test/scala/LayeredImageProcessorTest.scala
@@ -3,11 +3,27 @@ import org.scalatest.{FunSuite, Matchers}
 import scala.io.Source
 
 class LayeredImageProcessorTest extends FunSuite with Matchers {
-  test(s"Should match expected output for test case") {
+  test(s"Check should match expected output for test case") {
     new LayeredImageProcessor().doCheck(imageData = "123456789012", size = (3, 2)) should be (1)
   }
 
-  test(s"Should match expected output for input case") {
+  test(s"Decode should match expected output for test case") {
+    new LayeredImageProcessor().decodeImage(imageData = "0222112222120000", size = (2, 2)) should be (
+      """01
+        |10""".stripMargin)
+  }
+
+  test(s"Check should match expected output for input case") {
     new LayeredImageProcessor().doCheck(imageData = Source.fromResource("day8input.txt").getLines().next(), size = (25, 6)) should be (2413)
+  }
+
+  test(s"Decode should match expected output for input case") {
+    new LayeredImageProcessor().decodeImage(imageData = Source.fromResource("day8input.txt").getLines().next(), size = (25, 6)) should be (
+      """1110001100111001111011100
+        |1001010010100100001010010
+        |1110010000100100010011100
+        |1001010000111000100010010
+        |1001010010100001000010010
+        |1110001100100001111011100""".stripMargin)
   }
 }
